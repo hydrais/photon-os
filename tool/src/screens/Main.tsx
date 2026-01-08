f133b364f46604ab96f7293aed943c9860d51bac
Jesse Dunlap <jesse@1by3.co>
2026-01-07 18:37:47 -0600

    Added beginnings of tool app

---
+++ tool/src/screens/Main.tsx
@@ -0,0 +1,7 @@
+export function MainScreen() {
+  return (
+    <div className="fixed inset-0 bg-stone-200 flex flex-col items-center justify-center">
+      <div>Photon Tool</div>
+    </div>
+  );
+}
